681a33ed6b5bf9156ef53387efae6387a1045e5c
Erin Bassity <erin.bassity@cognizant.com>
2019-08-13 17:43:42 -0600

    Add seeds for users and nuggets

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Hipster.words(number: 1).first,
+    email: Faker::Internet.email
+  )
+  puts "You just created 10 user seeds!"
+end
+
+10.times do
+  Nugget.create!(
+    title: Faker::GreekPhilosophers.quote,
+    description: Faker::ChuckNorris.fact,
+    external_link: Faker::Internet.url,
+    user_id: rand(1..10)
+  )
+  puts "You just created 10 nuggets!"
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   context "username validation" do
     it "validates username is present" do
 
