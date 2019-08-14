bf0bafdbcb42d76a34955339e6f62cb5b579ee04
Erin Bassity <erin.bassity@cognizant.com>
2019-08-14 17:55:50 -0600

    add JsonWebToken encode and decode methods

--- spec/lib/json_web_token_spec.rb
+++ spec/lib/json_web_token_spec.rb
@@ -14,12 +14,13 @@ describe 'json web token encode' do
   end
 
   it 'decodes a token in response' do
-    token = JWT.decode({
-      :token => "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImphbmV0IiwidXNlcl9pZCI6MSwiZXhwIjo0fQ.akaACLZBQM6ukNC7CFm3wtTY7jnGkqjAxtT67QFko0g"
-    }, Rails.application.secrets.secret_key_base )
+    token = JWT.encode({
+      :username => 'janet',
+      :user_id => 1
+    }, Rails.application.secrets.secret_key_base)
 
-    fake_token = { :token => "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImphbmV0IiwidXNlcl9pZCI6MSwiZXhwIjo0fQ.akaACLZBQM6ukNC7CFm3wtTY7jnGkqjAxtT67QFko0g" }
+    decoded_token = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
 
-    expect(JsonWebToken.decode(fake_token)).to eq(token)
+    expect(JsonWebToken.decode(token)).to eq(decoded_token)
   end
 end
