3603237f8b7286a277cf23d66811177e3f75e84e
Amy Bertken <m_632767@Amys-MacBook-Pro.local>
2019-08-19 17:47:31 -0600

    add test for authenticate user

---
+++ spec/commands/authenticate_user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'authenticate user' do
+  it 'returns an encoded user_id when a user with valid credentials is submitted' do
+    user = create(:user)
+    command = AuthenticateUser.call(user.username, user.password)
+    
+    expect(command).to be_success
+    expect(JsonWebToken.decode(command.result)['user_id']).to eq(user.id)
+  end
+end
