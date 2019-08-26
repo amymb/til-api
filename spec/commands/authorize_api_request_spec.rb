29c4cf8d0da2eab4c1b535146bf07fcdc1887f4a
Erin Bassity <erin.bassity@cognizant.com>
2019-08-26 17:32:31 -0600

    Add spacing in test

--- spec/commands/authorize_api_request_spec.rb
+++ spec/commands/authorize_api_request_spec.rb
@@ -5,13 +5,14 @@ describe 'authorize api request' do
         user = create(:user)
         command = AuthorizeApiRequest.call({'Authorization' => 'Bearer ' + JsonWebToken.encode({:user_id => user.id})
         })
-        # binding.pry
+
         expect(command).to be_success
         expect(command.result).to eq(user)
     end
 
     it 'returns error message if invalid token' do
         command = AuthorizeApiRequest.call({'Authorization' => 'Bearer ' + JsonWebToken.encode({:user_id => 12 })})
+        
         expect(command).to_not be_success
         expect(command.result).to eq(nil)
         expect(command.errors[:token][0]).to eq('Invalid token')
